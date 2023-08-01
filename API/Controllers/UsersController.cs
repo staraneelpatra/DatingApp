cb327a5887bffa7b8f840226e8ba3b09fdcdf290
staraneelpatra <staraneel@live.com>
2023-08-02 00:07:06 +0530

    End of Section 4, Authenticatoin and Authorization

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,14 @@
 using API.Data;
 using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers{
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
@@ -17,6 +18,7 @@ namespace API.Controllers{
             
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult <IEnumerable<AppUser>>> GetUsers()
         {
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,39 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config){
+            // * Uses the Token Key placed in Config file to convert into symmentric key.
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+        public string CreateToken(AppUser user)
+        {
+            // * create a claim using Username
+            var claims = new List<Claim>{
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
