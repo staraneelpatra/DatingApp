1fa6c047f8a778d792198afc215d3a739778bc4e
staraneelpatra <staraneel@live.com>
2023-08-10 00:08:57 +0530

    Routing Basics

--- DatingAppClient/src/app/app.module.ts
+++ DatingAppClient/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
@@ -17,7 +21,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailsComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
