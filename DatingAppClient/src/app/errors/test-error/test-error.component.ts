d04cb249d316a6bc1b6a34d24693054caf67bceb
staraneelpatra <staraneel@live.com>
2023-08-11 23:09:53 +0530

    Separate Validation Error

--- DatingAppClient/src/app/errors/test-error/test-error.component.ts
+++ DatingAppClient/src/app/errors/test-error/test-error.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class TestErrorComponent {
 
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
 
   
   constructor(private http: HttpClient) {}
@@ -44,7 +45,10 @@ export class TestErrorComponent {
   get400ValidationError(){
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: response => console.log(response),
-      error: error => console.log(error)
+      error: error => {
+        console.log(error)
+        this.validationErrors=error;
+      }
     })
   }
 
